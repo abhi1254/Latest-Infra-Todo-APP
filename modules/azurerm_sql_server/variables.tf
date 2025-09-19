ef827bed791a3e61f732aac51aaefc6405a208a5
Abhishek Ojha <51531951+abhi1254@users.noreply.github.com>
2025-09-19 10:34:15 +0530

    Delete modules directory

--- modules/azurerm_sql_server/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "sql_server_name" {}
-variable "rg_name" {}
-variable "location" {}
-variable "admin_username" {}
-variable "admin_password" {}
-variable "tags" {}
-variable "audit_storage_endpoint" {}
-variable "audit_storage_key" {}
